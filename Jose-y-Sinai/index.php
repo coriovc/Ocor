f3d6506b42f8408c1682bff5a3b6877b5e6c6574
Victor corio <65185738+coriovc@users.noreply.github.com>
2025-02-11 14:26:47 -0400

    MOD

---
+++ Jose-y-Sinai/index.php
@@ -0,0 +1,305 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Jose y Sinai</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div  class="head_top">
+            <div class="container">
+               <div class="header">
+                  <div class="row">
+                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                           <div class="center-desk">
+                              <div class="logo">
+                                 <a href="index.html"><img src="images/logo.png" alt="#" /></a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                           <span class="navbar-toggler-icon"></span>
+                           </button>
+                           <div class="collapse navbar-collapse" id="navbarsExample04">
+                              <ul class="navbar-nav mr-auto">
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="#"> Inicio  </a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="#con">Confirmación</a>
+                                 </li>
+                              </ul>
+                           </div>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- end header inner -->
+            <!-- end header -->
+            <!-- banner -->
+            <section class="banner_main">
+               <div class="container">
+                  <div class="row d_flex">
+                     <div class="col-md-12">
+                        <div class="text-bg">
+                           <span> Bienvenido a nuestra historia de amor</span>
+                           <h1>Jose y Sinai</h1>
+                           <a href="#"> 30 de Abril, 2025</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </section>
+         </div>
+      </header>
+      <!-- end banner -->
+      <!-- weare -->
+      <div class="weare">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>¿Quienes Somos?</h2>
+                     <span>Hola, somos José y Sinaí, El 09 de septiembre nos conocimos y ocurrió el amor a primera vista, ambos lo sabíamos y Dios lo confirmó.</span>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                  <div class="row">
+                     <div class="col-md-6 margin_boo">
+                        <div class="weare_box ">
+                           <figure><img src="images/weare_img1.png" alt="#"/></figure>
+                        </div>
+                     </div>
+                     <div class="col-md-6">
+                        <div class="weare_box">
+                           <figure><img src="images/weare_img2.png" alt="#"/></figure>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-12">
+                  <div class="weare_box">
+                     <p>El 24 de Diciembre iniciamos este camino con mucha incertidumbre pero con una convicción de amor inquebrantable y, por supuesto, agarraditos de Dios.
+
+Si estás recibiendo esto es porque eres muy importante para nosotros y has sido parte nuestro proceso de crecimiento y la construcción de este gran amor que vamos a formalizar delante de Dios, quien sabemos que nos unió con un propósito gigante.</p>         
+          </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- weare -->
+      <!-- Our -->
+      <div id="" class="Our">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>¿Donde es y que necesitas saber?</h2>
+                     <span>Aquí te dejamos los datos necesarios que necesitas saber para nuestro día especial.</span>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-4">
+                  <div class="Our_box">
+                     <i><img src="images/plan1.png" alt="#"/></i>
+                     <h4>LUGAR</h4>
+                     <p>Callejon los cocos Qta claret #41 las delicias.</p>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="Our_box">
+                     <i><img src="images/plan2.png" alt="#"/></i>
+                     <h4>Vestimenta</h4>
+                     <p>Formal Garden party</p>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="Our_box">
+                     <i><img src="images/plan3.png" alt="#"/></i>
+                     <h4>Regalos</h4>
+                     <p>Los obsequios en efectivo siempre son bienvenidos.
+Deposita tu sobre en el cofre que encontrarás el día del evento.
+
+Para transferencias bancarias: 
+
+Bancamiga 
+04243041780
+27453944</p>
+                  </div>
+               </div>
+               <div class="col-md-12">
+                  <a class="read_more" href="https://maps.app.goo.gl/cEU6dpQCFmgtVtMf7">Ubicación por mapa.</a>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end Our -->
+      <!-- regist -->
+      <div id="regist" class="regist">
+         <div class="container" id="con">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>¿Vendrás?</h2>
+                     <span>Nos encantaría formaras parte de esta experiencía, en el siguiente boto te enviará al whatsapp para que nos mande el mensaje de confirmácion. Nota: Debes llenar el formulario dentro del mensaje. </span>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 ">
+                  <form class="main_form">
+                     <div class="row">
+               <div class="col-md-12 ">
+                  <a class="read_more" href="https://wa.link/w0v7pf">Para confirmar presiona aquí.</a>
+               </div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end regist -->
+      <!-- testimonial -->
+      <div class="testimonial">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>Notas importantes.</h2>
+                     <p>Tenemos algunas reglas para nuestro día especíal.<br> </p>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-12">
+                  <div id="myCarousel" class="carousel slide testimonial_Carousel " data-ride="carousel">
+                     <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                     </ol>
+                     <div class="carousel-inner">
+                        <div class="carousel-item active">
+                           <div class="container">
+                              <div class="carousel-caption ">
+                                 <div class="row">
+                                    <div  class="col-md-6">
+                                       <div class="test_box">
+                                          <div class="jons">
+                                             <i><img src="images/jons_img1.png" alt="#"/></i>
+                                             <h4>Sinai.</h4>
+                                          </div>
+                                          <p>Amamos a sus chiquitos, pero nos gustaría que pudieran disfrutar con nosotros al máximo de nuestro dia, mis niñitos a dormir</p>
+                                       </div>
+                                    </div>
+                                    <div  class="col-md-6">
+                                       <div class="test_box">
+                                          <div class="jons">
+                                             <i><img src="images/jons_img2.png" alt="#"/></i>
+                                             <h4>Jose</h4>
+                                          </div>
+                                          <p>Sabemos que todos quieren compartir, pero por favor respetemos las ubicaciones, cada quien en su mesa</p>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                       
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end testimonial -->
+      <!--  footer -->
+      <footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-10 offset-md-1">
+                     <div class="cont">
+                        <h3> <strong class="multi"> ¡Te esperamos!</strong><br>
+                           Para nuesto primer capitulo de amor 
+                        </h3>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>Sina, espero la invitación te haya gustado, felicidades. <a href="https://html.design/"> Att: Thomas Moreno. </a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+   </body>
+</html>
+
